1ba7360874bb69974169ae8a610ed04749cea7fb
Li Shi <shili2048@gmail.com>
2023-03-25 04:19:58 +0000

    bus: add uartlite device

--- src/main/scala/UART.scala
+++ src/main/scala/UART.scala
@@ -6,8 +6,8 @@ import freechips.rocketchip.tilelink._
 import difftest._
 
 class UART(implicit p: Parameters) extends LazyModule with HasCherrySpringsParameters {
-  val device    = new SimpleDevice("uart", Seq("uart-0"))
-  val beatBytes = 8
+  val device    = new SimpleDevice("uartlite", Seq("uartlite-0"))
+  val beatBytes = 4
   val node = TLManagerNode(
     Seq(
       TLSlavePortParameters.v1(
@@ -25,6 +25,9 @@ class UART(implicit p: Parameters) extends LazyModule with HasCherrySpringsParam
       )
     )
   )
+
+  val RxFifoDepth = 4
+  val TxFifoDepth = 4
 
   lazy val module = new LazyModuleImp(this) {
     val io         = IO(new UARTIO)
@@ -51,22 +54,91 @@ class UART(implicit p: Parameters) extends LazyModule with HasCherrySpringsParam
     val is_put   = (req.opcode === TLMessages.PutFullData) || (req.opcode === TLMessages.PutPartialData)
     val req_r    = RegInit(0.U.asTypeOf(tl.a.bits))
     val is_get_r = RegInit(false.B)
+    val is_put_r = RegInit(false.B)
+    val index_r  = req_r.address(3, 2)
 
     when(tl.a.fire) {
       req_r    := tl.a.bits
       is_get_r := is_get
+      is_put_r := is_put
     }
 
-    val rdata       = RegInit(0.U(8.W))
-    val valid_inout = tl.a.fire && (req.address(15, 0) === "h0000".U)
+    val rx = Module(new Queue(UInt(8.W), entries = RxFifoDepth, hasFlush = true))
+    val tx = Module(new Queue(UInt(8.W), entries = TxFifoDepth, hasFlush = true))
 
-    // output is valid when a channel fire
-    io.out.valid := valid_inout && is_put
-    io.out.ch    := req.data
+    val rdata = WireDefault(0.U(32.W))
 
-    // input is received when a channel fire
-    io.in.valid := valid_inout && is_get
-    rdata       := io.in.ch
+    // 0x0, read-only
+    val rx_fifo = Wire(UInt(32.W))
+    rx.io.deq.ready := false.B
+    rx_fifo         := rx.io.deq.bits
+    when(index_r === 0.U) {
+      rdata := rx_fifo
+      when(tl.d.fire) {
+        rx.io.deq.ready := true.B
+      }
+    }
+
+    // 0x4, write-only
+    val tx_fifo = WireDefault(0.U(32.W))
+    tx.io.enq.bits  := tx_fifo
+    tx.io.enq.valid := false.B
+    when(index_r === 1.U) {
+      tx_fifo := req_r.data
+      when(tl.d.fire && is_put_r) {
+        tx.io.enq.valid := true.B
+      }
+    }
+
+    // 0x8, read-only
+    val status                    = Wire(UInt(32.W))
+    val status_rx_fifo_valid_data = Wire(Bool())
+    val status_rx_fifo_full       = Wire(Bool())
+    val status_tx_fifo_empty      = Wire(Bool())
+    val status_tx_fifo_full       = Wire(Bool())
+    status_rx_fifo_valid_data := rx.io.deq.valid
+    status_rx_fifo_full       := (rx.io.count === RxFifoDepth.U)
+    status_tx_fifo_empty      := (tx.io.count === 0.U)
+    status_tx_fifo_full       := (tx.io.count === TxFifoDepth.U)
+    status := Cat(
+      0.U(28.W),
+      status_tx_fifo_full,
+      status_tx_fifo_empty,
+      status_rx_fifo_full,
+      status_rx_fifo_valid_data
+    )
+    when(index_r === 2.U) {
+      rdata := status
+    }
+
+    // 0xC, write-only
+    rx.io.flush.get := false.B
+    tx.io.flush.get := false.B
+    when(index_r === 3.U) {
+      when(tl.d.fire && is_put_r) {
+        // reset tx fifo
+        when(req_r.data(0)) {
+          tx.io.flush.get := true.B
+        }
+        // reset rx fifo
+        when(req_r.data(1)) {
+          rx.io.flush.get := true.B
+        }
+      }
+    }
+
+    // output
+    tx.io.deq.ready := true.B
+    io.out.valid    := tx.io.deq.valid
+    io.out.ch       := tx.io.deq.bits
+    when(io.out.valid) {
+      printf("%c", io.out.ch)
+    }
+
+    // input
+    rx.io.enq.valid := false.B
+    rx.io.enq.bits  := 0.U
+    io.in.valid     := false.B
 
     tl.a.ready := (state === s_req)
     tl.d.valid := (state === s_resp)
