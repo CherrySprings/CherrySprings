ed37950bcf72d87b6a3184912ca7bfb4622a7577
Li Shi <shili2048@gmail.com>
2023-04-07 00:33:56 +0000

    lsu: misaligned load/store

--- src/main/scala/LSU.scala
+++ src/main/scala/LSU.scala
@@ -38,8 +38,11 @@ class LSU(implicit p: Parameters) extends CherrySpringsModule {
   val rdata_amo = RegInit(0.U(xLen.W))
   val wdata_amo = Wire(UInt(xLen.W))
 
-  val wdata = (Mux(state === s_req, io.wdata, wdata_amo) << (addr_offset << 3))(xLen - 1, 0)
-  val wmask = (MuxLookup(
+  val wdata = Wire(UInt(128.W))
+  val wmask = Wire(UInt(16.W))
+
+  wdata := Mux(state === s_req, io.wdata, wdata_amo) << (addr_offset << 3)
+  wmask := MuxLookup(
     io.uop.mem_len,
     0.U,
     Array(
@@ -48,11 +51,16 @@ class LSU(implicit p: Parameters) extends CherrySpringsModule {
       s"b$MEM_WORD".U  -> "b00001111".U(8.W),
       s"b$MEM_DWORD".U -> "b11111111".U(8.W)
     )
-  ) << addr_offset)(xLen / 8 - 1, 0)
+  ) << addr_offset
 
-  req.bits.addr  := io.addr
-  req.bits.wdata := wdata
-  req.bits.wmask := wmask
+  // misaligned memory access - cross 64-bit line access
+  // programmer needs to guarantee no misaligned MMIO
+  val cross_line   = RegEnable(wmask(15, 8) =/= 0.U, false.B, state === s_idle)
+  val current_line = RegInit(0.U(1.W))
+
+  req.bits.addr  := Mux(current_line === 0.U, io.addr, io.addr + 8.U)
+  req.bits.wdata := Mux(current_line === 0.U, wdata(63, 0), wdata(127, 64))
+  req.bits.wmask := Mux(current_line === 0.U, wmask(7, 0), wmask(15, 8))
   req.bits.len   := io.uop.mem_len // only used for uncache
   req.bits.wen   := io.is_store || (state === s_amo_st_req)
   req.valid      := (state === s_req) || (state === s_amo_ld_req) || (state === s_amo_st_req)
@@ -84,13 +92,16 @@ class LSU(implicit p: Parameters) extends CherrySpringsModule {
 
   switch(state) {
     is(s_idle) {
+      current_line := 0.U
       when(io.is_mem) {
-        when(misaligned) {
-          state := s_exc
-        }.elsewhen(is_sc) {
-          state := Mux(sc_succeed, s_req, s_resp)
+        when(is_lr || is_sc || io.is_amo) {
+          when(misaligned) {
+            state := s_exc
+          }.otherwise {
+            state := Mux(is_sc, Mux(sc_succeed, s_req, s_resp), Mux(io.is_amo, s_amo_ld_req, s_req))
+          }
         }.otherwise {
-          state := Mux(io.is_amo, s_amo_ld_req, s_req)
+          state := s_req
         }
       }
     }
@@ -101,7 +112,12 @@ class LSU(implicit p: Parameters) extends CherrySpringsModule {
     }
     is(s_resp) {
       when(resp.fire || sc_completed) {
-        state := Mux(resp.bits.page_fault || resp.bits.access_fault, s_exc, s_idle)
+        when(resp.bits.page_fault || resp.bits.access_fault) {
+          state := s_exc
+        }.otherwise {
+          state        := Mux(cross_line && (current_line === 0.U), s_req, s_idle)
+          current_line := current_line + 1.U
+        }
       }
     }
     is(s_amo_ld_req) {
@@ -130,41 +146,47 @@ class LSU(implicit p: Parameters) extends CherrySpringsModule {
   }
 
   // todo: optimize this logic
-  val wdata_raw = Mux(io.uop.mem_len === s"b$MEM_DWORD".U, io.wdata, SignExt32_64(io.wdata(31, 0)))
+  val wdata_amo_raw = Mux(io.uop.mem_len === s"b$MEM_DWORD".U, io.wdata, SignExt32_64(io.wdata(31, 0)))
   wdata_amo := MuxLookup(
     io.uop.lsu_op,
     0.U,
     Array(
-      s"b$LSU_AMOSWAP".U -> wdata_raw,
-      s"b$LSU_AMOADD".U  -> (wdata_raw + rdata_amo),
-      s"b$LSU_AMOAND".U  -> (wdata_raw & rdata_amo),
-      s"b$LSU_AMOOR".U   -> (wdata_raw | rdata_amo),
-      s"b$LSU_AMOXOR".U  -> (wdata_raw ^ rdata_amo),
-      s"b$LSU_AMOMAX".U  -> Mux(wdata_raw.asSInt > rdata_amo.asSInt, wdata_raw, rdata_amo),
-      s"b$LSU_AMOMAXU".U -> Mux(wdata_raw.asUInt > rdata_amo.asUInt, wdata_raw, rdata_amo),
-      s"b$LSU_AMOMIN".U  -> Mux(wdata_raw.asSInt < rdata_amo.asSInt, wdata_raw, rdata_amo),
-      s"b$LSU_AMOMINU".U -> Mux(wdata_raw.asUInt < rdata_amo.asUInt, wdata_raw, rdata_amo)
+      s"b$LSU_AMOSWAP".U -> wdata_amo_raw,
+      s"b$LSU_AMOADD".U  -> (wdata_amo_raw + rdata_amo),
+      s"b$LSU_AMOAND".U  -> (wdata_amo_raw & rdata_amo),
+      s"b$LSU_AMOOR".U   -> (wdata_amo_raw | rdata_amo),
+      s"b$LSU_AMOXOR".U  -> (wdata_amo_raw ^ rdata_amo),
+      s"b$LSU_AMOMAX".U  -> Mux(wdata_amo_raw.asSInt > rdata_amo.asSInt, wdata_amo_raw, rdata_amo),
+      s"b$LSU_AMOMAXU".U -> Mux(wdata_amo_raw.asUInt > rdata_amo.asUInt, wdata_amo_raw, rdata_amo),
+      s"b$LSU_AMOMIN".U  -> Mux(wdata_amo_raw.asSInt < rdata_amo.asSInt, wdata_amo_raw, rdata_amo),
+      s"b$LSU_AMOMINU".U -> Mux(wdata_amo_raw.asUInt < rdata_amo.asUInt, wdata_amo_raw, rdata_amo)
     )
   )
 
-  val resp_data = resp.bits.rdata >> (addr_offset << 3)
-  val sign = (!is_ldu && MuxLookup(
+  val rdata_raw_128 = Wire(UInt(128.W))
+  val rdata_raw     = Wire(UInt(64.W))
+  val rdata_sign    = Wire(UInt(1.W))
+  val rdata         = Wire(UInt(64.W))
+  val resp_data_r   = RegEnable(resp.bits.rdata, 0.U, resp.fire)
+  rdata_raw_128 := Mux(cross_line, Cat(resp.bits.rdata, resp_data_r), resp.bits.rdata)
+  rdata_raw     := rdata_raw_128 >> (addr_offset << 3)
+  rdata_sign := (!is_ldu && MuxLookup(
     io.uop.mem_len,
     false.B,
     Array(
-      s"b$MEM_BYTE".U -> resp_data(7),
-      s"b$MEM_HALF".U -> resp_data(15),
-      s"b$MEM_WORD".U -> resp_data(31)
+      s"b$MEM_BYTE".U -> rdata_raw(7),
+      s"b$MEM_HALF".U -> rdata_raw(15),
+      s"b$MEM_WORD".U -> rdata_raw(31)
     )
   ).asBool).asUInt
-  val rdata = MuxLookup(
+  rdata := MuxLookup(
     io.uop.mem_len,
     0.U,
     Array(
-      s"b$MEM_BYTE".U  -> Cat(Fill(56, sign), resp_data(7, 0)),
-      s"b$MEM_HALF".U  -> Cat(Fill(48, sign), resp_data(15, 0)),
-      s"b$MEM_WORD".U  -> Cat(Fill(32, sign), resp_data(31, 0)),
-      s"b$MEM_DWORD".U -> resp_data
+      s"b$MEM_BYTE".U  -> Cat(Fill(56, rdata_sign), rdata_raw(7, 0)),
+      s"b$MEM_HALF".U  -> Cat(Fill(48, rdata_sign), rdata_raw(15, 0)),
+      s"b$MEM_WORD".U  -> Cat(Fill(32, rdata_sign), rdata_raw(31, 0)),
+      s"b$MEM_DWORD".U -> rdata_raw
     )
   )
 
@@ -173,7 +195,8 @@ class LSU(implicit p: Parameters) extends CherrySpringsModule {
   }
 
   io.rdata := Mux(is_sc, (!sc_succeed).asUInt, Mux(io.is_amo, rdata_amo, rdata))
-  io.valid := (state === s_resp || state === s_amo_st_resp) && (resp.fire && !resp.bits.page_fault && !resp.bits.access_fault) ||
+  io.valid := (state === s_resp || state === s_amo_st_resp) &&
+    (resp.fire && !resp.bits.page_fault && !resp.bits.access_fault && Mux(cross_line, current_line === 1.U, true.B)) ||
     sc_completed || (state === s_exc) // assert for only 1 cycle
   io.is_mmio := io.valid && resp.bits.mmio
   io.ready   := ((state === s_idle) && !io.is_mem) || io.valid
@@ -192,14 +215,14 @@ class LSU(implicit p: Parameters) extends CherrySpringsModule {
     Mux(
       io.is_store || io.is_amo,
       Mux(
-        misaligned,
-        Causes.misaligned_store.U,
-        Mux(RegNext(resp.bits.access_fault), Causes.store_access.U, Causes.store_page_fault.U)
+        RegNext(resp.bits.access_fault),
+        Causes.store_access.U,
+        Mux(RegNext(resp.bits.page_fault), Causes.store_page_fault.U, Causes.misaligned_store.U)
       ),
       Mux(
-        misaligned,
-        Causes.misaligned_load.U,
-        Mux(RegNext(resp.bits.access_fault), Causes.load_access.U, Causes.load_page_fault.U)
+        RegNext(resp.bits.access_fault),
+        Causes.load_access.U,
+        Mux(RegNext(resp.bits.page_fault), Causes.load_page_fault.U, Causes.misaligned_load.U)
       )
     )
   io.exc_code := Mux(state === s_exc, exc_code, 0.U)
