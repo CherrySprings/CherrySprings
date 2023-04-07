2a91a32e94d4efcbf13deeb5a73963e9007dca5e
Li Shi <shili2048@gmail.com>
2023-04-07 05:56:57 +0000

    system: start to implement multicore

--- src/main/scala/FPGA.scala
+++ src/main/scala/FPGA.scala
@@ -12,17 +12,17 @@ import java.nio.file._
 import testchipip._
 
 abstract class FPGAAbstract(implicit p: Parameters) extends LazyModule with HasCherrySpringsParameters {
-  val clint_int: IntIdentityNode
-  val plic_int:  IntIdentityNode
-  val node:      Option[TLIdentityNode]
+  val clint_int: Seq[IntIdentityNode]
+  val plic_int:  Seq[IntIdentityNode]
+  val node:      Option[Seq[TLIdentityNode]]
   override lazy val module = new FPGAAbstractImp(this)
 }
 
 class FPGAAbstractImp[+L <: FPGAAbstract](l: L) extends LazyModuleImp(l) with HasCherrySpringsParameters {
   val io = IO(new Bundle {
     val uart     = new UARTIO
-    val in       = if (enableSerdes) Some(Flipped(Decoupled(UInt(tlSerWidth.W)))) else None
-    val out      = if (enableSerdes) Some(Decoupled(UInt(tlSerWidth.W))) else None
+    val in       = if (enableSerdes) Some(Vec(numHarts, Flipped(Decoupled(UInt(tlSerWidth.W))))) else None
+    val out      = if (enableSerdes) Some(Vec(numHarts, Decoupled(UInt(tlSerWidth.W)))) else None
     val io_clock = if (enableSerdes) Some(Input(Clock())) else None
     val io_reset = if (enableSerdes) Some(Input(Bool())) else None
   })
@@ -35,7 +35,10 @@ class FPGAImp(implicit p: Parameters) extends FPGAAbstract {
   val plic              = LazyModule(new TLPLIC(PLICParams(), 8))
   val uart              = LazyModule(new UART)
   val mem               = LazyModule(new TLVirtualRam)
-  val node              = Some(TLIdentityNode())
+  val node = Some(for (i <- 0 until numHarts) yield {
+    val node = TLIdentityNode()
+    node
+  })
 
   // BootROM
   lazy val boot_rom_contents = {
@@ -58,8 +61,11 @@ class FPGAImp(implicit p: Parameters) extends FPGAAbstract {
   hart_id_allocator.node := xbar.node
   clint.node             := xbar.node
   plic.node              := xbar.node
-  uart.node              := TLFIFOFixer() := TLFragmenter(4, 8) := TLWidthWidget(8)    := xbar.node
-  xbar.node              := TLBuffer()    := TLFIFOFixer()      := TLFragmenter(8, 32) := node.get
+  uart.node              := TLFIFOFixer() := TLFragmenter(4, 8) := TLWidthWidget(8) := xbar.node
+
+  for (i <- 0 until numHarts) {
+    xbar.node := TLBuffer() := TLFIFOFixer() := TLFragmenter(8, 32) := node.get(i)
+  }
 
   class IntSourceNodeToModule(val num: Int)(implicit p: Parameters) extends LazyModule {
     val sourceNode = IntSourceNode(IntSourcePortSimple(num, ports = 1, sources = 1))
@@ -73,10 +79,12 @@ class FPGAImp(implicit p: Parameters) extends FPGAAbstract {
   plic.intnode := plicSource.sourceNode
 
   // interrupt sources
-  val clint_int = IntIdentityNode()
-  val plic_int  = IntIdentityNode()
-  clint_int := clint.intnode
-  plic_int :*= plic.intnode
+  val clint_int = for (i <- 0 until numHarts) yield { val n = IntIdentityNode(); n }
+  val plic_int  = for (i <- 0 until numHarts) yield { val n = IntIdentityNode(); n }
+  for (i <- 0 until numHarts) {
+    clint_int(i) := clint.intnode
+    plic_int(i) :*= plic.intnode
+  }
 
   override lazy val module = new FPGAAbstractImp(this) {
     // sync external interrupts
@@ -106,45 +114,54 @@ class FPGA(implicit p: Parameters) extends FPGAAbstract {
   val node = None
 
   // interrupt
-  val clint_int = IntIdentityNode()
-  val plic_int  = IntIdentityNode()
-  clint_int := fpga_imp.clint_int
-  plic_int :*= fpga_imp.plic_int
+  val clint_int = for (i <- 0 until numHarts) yield { val n = IntIdentityNode(); n }
+  val plic_int  = for (i <- 0 until numHarts) yield { val n = IntIdentityNode(); n }
+  for (i <- 0 until numHarts) {
+    clint_int(i) := fpga_imp.clint_int(i)
+    plic_int(i) :*= fpga_imp.plic_int(i)
+  }
 
   // desser
-  val desser = LazyModule(
-    new TLDesser(
-      w = tlSerWidth,
-      params = Seq(
-        TLMasterParameters.v1(
-          name     = "tl-desser",
-          sourceId = IdRange(0, 1 << tlSourceBits)
+  val desser = for (i <- 0 until numHarts) yield {
+    val desser = LazyModule(
+      new TLDesser(
+        w = tlSerWidth,
+        params = Seq(
+          TLMasterParameters.v1(
+            name     = "tl-desser",
+            sourceId = IdRange(0, 1 << tlSourceBits)
+          )
         )
       )
     )
-  )
-  fpga_imp.node.get := desser.node
+    desser
+  }
+  for (i <- 0 until numHarts) {
+    fpga_imp.node.get(i) := desser(i).node
+  }
 
   override lazy val module = new FPGAAbstractImp(this) {
-    val mergeType     = desser.module.mergeTypes(0)
+    val mergeType     = desser(0).module.mergeTypes(0)
     val wordsPerBeat  = (mergeType.getWidth - 1) / tlSerWidth + 1
     val beatsPerBlock = 4
     val qDepth        = (wordsPerBeat * beatsPerBlock) << tlSourceBits
 
-    val in_fifo  = Module(new AsyncQueue(UInt(tlSerWidth.W)))
-    val out_fifo = Module(new AsyncQueue(UInt(tlSerWidth.W)))
-    in_fifo.io.enq               <> io.in.get
-    in_fifo.io.enq_clock         := io.io_clock.get
-    in_fifo.io.enq_reset         := io.io_reset.get
-    desser.module.io.ser.head.in <> Queue(in_fifo.io.deq, qDepth)
-    in_fifo.io.deq_clock         := clock
-    in_fifo.io.deq_reset         := reset
-    out_fifo.io.enq              <> Queue(desser.module.io.ser.head.out, qDepth)
-    out_fifo.io.enq_clock        := clock
-    out_fifo.io.enq_reset        := reset
-    out_fifo.io.deq              <> io.out.get
-    out_fifo.io.deq_clock        := io.io_clock.get
-    out_fifo.io.deq_reset        := io.io_reset.get
+    for (i <- 0 until numHarts) {
+      val in_fifo  = Module(new AsyncQueue(UInt(tlSerWidth.W)))
+      val out_fifo = Module(new AsyncQueue(UInt(tlSerWidth.W)))
+      in_fifo.io.enq                  <> io.in.get(i)
+      in_fifo.io.enq_clock            := io.io_clock.get
+      in_fifo.io.enq_reset            := io.io_reset.get
+      desser(i).module.io.ser.head.in <> Queue(in_fifo.io.deq, qDepth)
+      in_fifo.io.deq_clock            := clock
+      in_fifo.io.deq_reset            := reset
+      out_fifo.io.enq                 <> Queue(desser(i).module.io.ser.head.out, qDepth)
+      out_fifo.io.enq_clock           := clock
+      out_fifo.io.enq_reset           := reset
+      out_fifo.io.deq                 <> io.out.get(i)
+      out_fifo.io.deq_clock           := io.io_clock.get
+      out_fifo.io.deq_reset           := io.io_reset.get
+    }
 
     // UART
     io.uart <> fpga_imp.module.io.uart
