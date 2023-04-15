27daac711f236019e60e7a38a0d00d46ae1f7af5
Li Shi <shili2048@gmail.com>
2023-04-15 06:40:11 +0000

    rf: optimize reg file logic

--- src/main/scala/RegFile.scala
+++ src/main/scala/RegFile.scala
@@ -14,15 +14,19 @@ class RegFile(implicit p: Parameters) extends CherrySpringsModule {
     val rf_a0     = Output(UInt(8.W)) // only for difftest
   })
 
-  val rf = RegInit(VecInit(Seq.fill(32)(0.U(xLen.W))))
+  def access(x: UInt) = rf(~x)
+  def access(i: Int)  = rf(31 - i)
+
+  val rf = RegInit(VecInit(Seq.fill(31)(0.U(xLen.W))))
 
   when(io.rd_wen && (io.rd_index =/= 0.U)) {
-    rf(io.rd_index) := io.rd_data;
+    access(io.rd_index) := io.rd_data;
   }
 
-  io.rs1_data := Mux(io.rs1_index =/= 0.U, rf(io.rs1_index), 0.U)
-  io.rs2_data := Mux(io.rs2_index =/= 0.U, rf(io.rs2_index), 0.U)
+  io.rs1_data := Mux(io.rs1_index =/= 0.U, access(io.rs1_index), 0.U)
+  io.rs2_data := Mux(io.rs2_index =/= 0.U, access(io.rs2_index), 0.U)
 
+  // bypass
   when(io.rd_wen && (io.rd_index =/= 0.U)) {
     when(io.rd_index === io.rs1_index) {
       io.rs1_data := io.rd_data
@@ -37,9 +41,10 @@ class RegFile(implicit p: Parameters) extends CherrySpringsModule {
     val dt_ar = Module(new DifftestArchIntRegState)
     dt_ar.io.clock  := clock
     dt_ar.io.coreid := hartID.U
-    for (i <- 0 until 32) {
-      dt_ar.io.gpr(i) := Mux(io.rd_wen && (io.rd_index === i.U) && (io.rd_index =/= 0.U), io.rd_data, rf(i))
+    dt_ar.io.gpr(0) := 0.U
+    for (i <- 1 until 32) {
+      dt_ar.io.gpr(i) := Mux(io.rd_wen && (io.rd_index === i.U) && (io.rd_index =/= 0.U), io.rd_data, access(i))
     }
-    io.rf_a0 := rf(10)(7, 0)
+    io.rf_a0 := access(10)(7, 0)
   }
 }
