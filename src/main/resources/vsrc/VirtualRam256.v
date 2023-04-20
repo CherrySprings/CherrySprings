041896626dfd0dc3171416b82f0016e5f64af940
Li Shi <shili2048@gmail.com>
2023-04-20 20:40:58 +0000

    ram: add dpi-c functions

--- src/main/resources/vsrc/VirtualRam256.v
+++ src/main/resources/vsrc/VirtualRam256.v
@@ -8,6 +8,20 @@ module VirtualRam256 (
   input          wen
 );
 
+import "DPI-C" function void ram_write_helper
+(
+  input  longint    wIdx,
+  input  longint    wdata,
+  input  longint    wmask,
+  input  bit        wen
+);
+
+import "DPI-C" function longint ram_read_helper
+(
+  input  bit        en,
+  input  longint    rIdx
+);
+
   wire [255:0] wmask256;
 
   assign rdata = {ram_read_helper(en, {addr, 2'b11}), ram_read_helper(en, {addr, 2'b10}),
